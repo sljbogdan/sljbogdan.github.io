cb061b2d790d6c8db0ab9bbbd9e995180c8c85de
Salajan Bogdan <salajan.bogdan_a@yahoo.com>
2023-01-10 23:13:15 +0200

    showPages simplified

--- function.js
+++ function.js
@@ -1,54 +1,50 @@
-function hidePage(id) {
-    console.info('hide page', id);
+function hide(id) {
     document.getElementById(id).style.display='none';
 }
 
 function highlight(page){
-        page.style.borderColor = '#0000ff';
+        page.style.borderColor = '#000099';
     setTimeout(function(){
-        page.style.borderColor = '#e099dd';
+        page.style.borderColor = '#000077';
+    }, 100);
+    setTimeout(function(){
+        page.style.borderColor = '#000055';
+    }, 150);
+    setTimeout(function(){
+        page.style.borderColor = '#000033';
     }, 200);
     setTimeout(function(){
         page.style.borderColor = '#e0dfdc';
     }, 300);
 }
 
-function showPage(show) {
-    console.info('show page', show);
-    var page = document.getElementById(show);
-    console.info('show', page);
+function show(id) {
+    var page = document.getElementById(id);
     if(page){
         page.style.display='block';
         highlight(page);
     } else{
-        console.warm('pagina cu id-ul %s nu exista', show);
+        console.warm('pagina cu id-ul %s nu exista', id);
     }
 }
 
-function showHome() {
-    hidePage('skills');
-    hidePage('projects');
-    hidePage('languages');
-    showPage('home');
+function hideAllPages(){
+    var pages = Array.from(document.getElementsByClassName('page'));
+    pages.forEach(function(page){
+        hide(page.id);
+    });
+
+    // while(i < pageIds.length){
+    //     hidePage(pageIds[i++]);
+    // }
+
+    // for(var i = 0; i < pageIds.length; i++){
+    //     hidePage(pageIds[i]);
+    // }
 }
 
-function showSkills() {
-    hidePage('home');
-    hidePage('projects');
-    hidePage('languages');
-    showPage('skills');
-}
+function showPage(id){
+    hideAllPages();
+    show(id);
 
-function showProjects() {
-    hidePage('home');
-    hidePage('skills');
-    hidePage('languages');
-    showPage('projects');
-}
-
-function showLanguages() {
-    hidePage('home');
-    hidePage('skills');
-    hidePage('projects');
-    showPage('languages');
 }
